384a99f8b8693b5e02c280b075de3f8bb18a1575
kaioenzo <kaioenzobr@gmail.com>
2021-10-02 13:09:24 -0300

    Adição da grid de jogos

---
+++ js/js.js
@@ -0,0 +1,5 @@
+document.getElementById("veja-mais").onclick = function() {
+    document.getElementById("mais-jogos").style['display'] = "block";
+    document.getElementById("veja-mais").style['display'] = "none";
+    
+}
