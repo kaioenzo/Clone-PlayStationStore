28488e712fbc314715f36a39b49b6a291adac9ea
kaioenzo <kaioenzobr@gmail.com>
2021-10-20 19:48:24 -0300

    Grid de categorias, rodapé, melhorias na responsividade

--- js/js.js
+++ js/js.js
@@ -1,5 +1,10 @@
-document.getElementById("veja-mais").onclick = function() {
-    document.getElementById("mais-jogos").style['display'] = "block";
-    document.getElementById("veja-mais").style['display'] = "none";
+document.getElementById("veja-mais-1").onclick = function() {
+    document.getElementById("mais-jogos-1").style['display'] = "block";
+    document.getElementById("veja-mais-1").style['display'] = "none";
+    
+}
+document.getElementById("veja-mais-2").onclick = function() {
+    document.getElementById("mais-jogos-2").style['display'] = "block";
+    document.getElementById("veja-mais-2").style['display'] = "none";
     
 }
